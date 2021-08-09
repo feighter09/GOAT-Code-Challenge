1d35a560bd787639c4c1016c453e5439e3863552
Austin Feight <dev@austinfeight.com>
2021-08-09 17:06:21 -0400

    Initial Commit

---
+++ GOAT Code Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOAT Code Challenge
+//
+//  Created by Austin Feight on 8/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
